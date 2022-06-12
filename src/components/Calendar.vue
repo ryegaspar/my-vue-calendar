a74b2670dac0ba165878a9cce52e1f6819b872f3
rye gaspar <gaspar.ryan@outlook.com>
2022-06-12 17:50:53 -0500

    add header functionality

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,26 +1,38 @@
 <template>
-	<div>
+	<div class="p-6">
 		<div>
-			<div>
-				<a href="#"
-				   @click.prevent="showModal"
-				>
-					show modal
-				</a>
-			</div>
-			<div>
-				<button>Today</button>
-				<div>
-					<button>Previous</button>
-					<button>Next</button>
-				</div>
-				<span>{{ formattedSelectedMonth }}</span>
-			</div>
-			<div v-for="(date, index) in calendarDates"
-				 :key="index"
+			<a href="#"
+			   @click.prevent="showModal"
 			>
-				{{ date.getDate() }} - {{ format(date, 'E') }}
+				show modal
+			</a>
+		</div>
+		<div class="flex p-2">
+			<div class="flex align-middle">
+				<button class="p-2 mr-1 text-white bg-blue-500 rounded border border-gray-300"
+						@click.prevent="today"
+				>
+					Today
+				</button>
+				<div>
+					<button class="p-2 mr-1 text-white bg-blue-500 rounded border border-gray-300"
+							@click.prevent="prevMonth"
+					>
+						Previous
+					</button>
+					<button class="p-2 mr-1 text-white bg-blue-500 rounded border border-gray-300"
+							@click.prevent="nextMonth"
+					>
+						Next
+					</button>
+				</div>
 			</div>
+			<span class="p-2">{{ formattedSelectedMonth }}</span>
+		</div>
+		<div v-for="(date, index) in calendarDates"
+			 :key="index"
+		>
+			{{ date.getDate() }} - {{ format(date, 'E') }}
 		</div>
 
 		<modal :show="isShow"
@@ -65,7 +77,7 @@
 
 			<div>
 				<input type="text"
-					  name="some_input"
+					   name="some_input"
 				/>
 			</div>
 		</modal>
@@ -74,12 +86,14 @@
 
 <script>
 import {
+	addMonths,
 	eachDayOfInterval,
 	endOfMonth,
 	endOfWeek,
 	format,
 	startOfMonth,
-	startOfWeek
+	startOfWeek,
+	subMonths
 } from 'date-fns'
 
 import modal from '@/components/Modal'
@@ -93,7 +107,6 @@ export default {
 	data() {
 		return {
 			selectedMonth: Date.now(),
-			calendarDates: null,
 			isShow: false
 		}
 	},
@@ -102,28 +115,38 @@ export default {
 		formattedSelectedMonth() {
 			return format(this.selectedMonth, 'MMMM yyyy')
 		},
-	},
 
-	mounted() {
-		this.calendarDates = this.getCalendarDates(this.selectedMonth)
-	},
-
-	methods: {
-		getCalendarDates(date) {
-			const firstWeekStart = startOfWeek(startOfMonth(date), { weekStartsOn: 1 })
-			const lastWeekStart = endOfWeek(endOfMonth(date), { weekStartsOn: 1 })
+		calendarDates() {
+			const firstWeekStart = startOfWeek(startOfMonth(this.selectedMonth), { weekStartsOn: 1 })
+			const lastWeekStart = endOfWeek(endOfMonth(this.selectedMonth), { weekStartsOn: 1 })
 			return eachDayOfInterval({
 										 start: firstWeekStart,
 										 end: lastWeekStart
 									 })
 		},
+	},
 
+	mounted() {},
+
+	methods: {
 		format(date, strFormat) {
 			return format(date, strFormat)
 		},
 
 		showModal() {
 			this.isShow = !this.isShow
+		},
+
+		nextMonth() {
+			this.selectedMonth = addMonths(this.selectedMonth, 1)
+		},
+
+		prevMonth() {
+			this.selectedMonth = subMonths(this.selectedMonth, 1)
+		},
+
+		today() {
+			this.selectedMonth = Date.now()
 		}
 	}
 }
