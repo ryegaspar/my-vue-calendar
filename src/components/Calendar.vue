0bed5184f5be1ccb837ff68cdb8eca378a6b9635
rye gaspar <gaspar.ryan@outlook.com>
2022-06-11 19:19:29 -0500

    add date-fns, calendar component

---
+++ src/components/Calendar.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<div>
+			<button>Today</button>
+			<div>
+				<button>Previous</button>
+				<button>Next</button>
+			</div>
+			<span>{{ formattedSelectedMonth }}</span>
+		</div>
+		<div>days</div>
+	</div>
+</template>
+
+<script>
+import {format} from 'date-fns'
+export default {
+	name: 'HelloWorld',
+	props: {
+		msg: String
+	},
+	data() {
+		return {
+			selectedMonth: Date.now()
+		}
+	},
+	computed: {
+		formattedSelectedMonth() {
+			return format(this.selectedMonth, "MMMM yyyy")
+		}
+	}
+}
+</script>
