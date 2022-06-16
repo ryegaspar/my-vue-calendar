76eb9e93455faad25cfe07dcf183cc6afb26d4d3
rye gaspar <gaspar.ryan@outlook.com>
2022-06-15 19:59:47 -0500

    add test store date ranged events

--- src/tests/datetest.test.js
+++ src/tests/datetest.test.js
@@ -1,18 +1,43 @@
 import DateEvent from '@/DateEvent'
 
-describe('DateEvent tests', () => {
-	const dateEvent = new DateEvent()
+const event = {
+	allDay: false,
+	endDate: '2000-01-01',
+	startDate: '2000-01-01',
+	eventDescription: 'a new event!'
+}
 
-	const event = {
-		allDay: false,
-		endDate: '2000-01-01',
-		startDate: '2000-01-01',
-		eventDescription: 'a new event!'
+const fakeLocalStorage = (function () {
+	let store = {}
+
+	return {
+		getItem: function (key) {
+			return store[key] || null
+		},
+		setItem: function (key, value) {
+			store[key] = value.toString()
+		},
+		removeItem: function (key) {
+			delete store[key]
+		},
+		clear: function () {
+			store = {}
+		}
 	}
+})()
+
+describe('DateEvent tests', () => {
+	let dateEvent
 
 	function getStorage(){
-		return JSON.parse(localStorage.getItem(dateEvent.localKey))
+		return JSON.parse(fakeLocalStorage.getItem(dateEvent.localKey))
 	}
+
+	beforeEach(() => {
+		Object.defineProperty(window, 'localStorage', { value: fakeLocalStorage })
+
+		dateEvent = new DateEvent()
+	})
 
 	it('it can add an event and store in local storage', () => {
 		dateEvent.addEvent(event)
@@ -29,8 +54,32 @@ describe('DateEvent tests', () => {
 						   })
 	})
 
-	it('test', () => {
-		console.log(getStorage())
-	})
+	it('it can add an event that ranges more than a day', () => {
+		localStorage.clear()
+		dateEvent.addEvent({
+			...event,
+			...{ endDate: '2000-01-02' }
+		})
 
+		const storage1 = getStorage()[0]
+		const storage2 = getStorage()[1]
+
+		expect(storage1)
+			.toMatchObject({
+							   allDay: false,
+							   endDate: '2000-01-02',
+							   startDate: '2000-01-01',
+							   eventDescription: 'a new event!',
+							   date: '2000-01-01'
+						   })
+
+		expect(storage2)
+			.toMatchObject({
+							   allDay: false,
+							   endDate: '2000-01-02',
+							   startDate: '2000-01-01',
+							   eventDescription: 'a new event!',
+							   date: '2000-01-02'
+						   })
+	})
 })
